9f6288e8d00924a0fe625be99add93d43ea1f5e9
rizalahmads <rizal.ahmad2012@gmail.com>
2022-11-23 19:53:12 +0700

    P11_PW

---
+++ Latihan/script.js
@@ -0,0 +1,10 @@
+document.writeln("<center>");
+document.writeln("<hr/>");
+document.writeln("<h1>ESQBS</h1>");
+document.writeln("<h2>Jurusan Ilmu Komputer</h2>");
+document.writeln("<hr/>");
+document.writeln("</center>");
+
+var data = 100;
+var ipk = 3.5;
+var hutang = -300;
